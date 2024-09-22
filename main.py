fb4fbb2d9f1c80fc78c474bdd1883e5669ae23cc
i-ohorodnyk <illyaohorodnyk@gmail.com>
2024-09-22 20:25:02 +0300

    Add fill_delta_by_rows functionality.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def remove_duplicates_from_ndarray(a):
     """
     # Type validation section
     assert type(a) == np.ndarray
+    assert a.shape == (9, )
 
     for i in range(len(a)):
         for j in range(i+1, len(a)):
@@ -21,6 +22,9 @@ def remove_duplicates_from_groups(field):
         This function takes the numpy.ndarray object with two dimenstion shape.
         This function returns None. 
     """
+    assert type(field) == np.ndarray
+    assert field.shape == (9, 9)
+
     a = np.zeros(9, dtype=np.int64)
     for i in range(0, 9, 3):
         for j in range(0, 9, 3):
@@ -34,5 +38,25 @@ def remove_duplicates_from_groups(field):
                 row[j:j+3] = a[c:c+3]
                 c += 3
 
+def fill_delta_by_rows(field):
+    """
+        This function replace digitals of type numpy.int64 that have value 0
+        to digit that have occurance in row of field. This method can generate
+        duplicates by columns order.
+        This function takes the numpy.ndarray object with two dimenstion shape.
+        This function returns None.
+    """
+    assert type(field) == np.ndarray
+    assert field.shape == (9, 9)
+
+    for i in range(field.shape[0]):
+        d = get_delta(field[i])
+        c = 0
+        for j in range(field.shape[1]):
+            if field[i][j] == np.int64(0):
+                field[i][j] = d[c]
+                c += 1
+
 get_field = lambda : np.array([np.array([rnd.randint(1, 9) for j in range(9)]) for i in range(9)])
+get_delta = lambda a: np.array([np.int64(i) for i in set(range(1, 10)) - set(a)], dtype=np.int64)
 
