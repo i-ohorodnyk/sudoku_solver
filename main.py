b695b59cdfa64b62fa5b1a48b1144259a6d81a21
i-ohorodnyk <illyaohorodnyk@gmail.com>
2024-09-25 21:20:37 +0300

    Rename fill_delta_by_rows to fill_delta_by_ndarray

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def remove_duplicates_from_groups(field):
         This function takes the numpy.ndarray object with two dimenstion shape.
         This function returns None. 
     """
+    # Type validation section
     assert type(field) == np.ndarray
     assert field.shape == (9, 9)
 
@@ -38,25 +39,42 @@ def remove_duplicates_from_groups(field):
                 row[j:j+3] = a[c:c+3]
                 c += 3
 
-def fill_delta_by_rows(field):
+def fill_delta_by_ndarray(a):
     """
         This function replace digitals of type numpy.int64 that have value 0
-        to digit that have occurance in row of field. This method can generate
+        to digit that have occurance in ndarray of field. This method can generate
         duplicates by columns order.
         This function takes the numpy.ndarray object with two dimenstion shape.
         This function returns None.
     """
-    assert type(field) == np.ndarray
-    assert field.shape == (9, 9)
+    # Type validation section
+    assert type(a) == np.ndarray
+    assert a.shape == (9, )
 
-    for i in range(field.shape[0]):
-        d = get_delta(field[i])
+    for i in range(9):
+        d = get_delta(a)
         c = 0
-        for j in range(field.shape[1]):
-            if field[i][j] == np.int64(0):
-                field[i][j] = d[c]
-                c += 1
+        for i in range(9)
+            if a[i] == 0:
+                [i] = d[c]
+                [c] += 1
+
+def get_delta(a):
+    """
+        This function complutes missing from range 1 to 9 numpy.int64 digitals
+        and returns this as numpy.ndarray of shape of (9, ).
+        This function takes the numpy.ndarray object with (9,) shape of 
+        numpy.int64 digitals that have no duplicates.
+        This function returns delta - numpy.ndarray that represent digitals
+        that need to replace from 0 to get filled numpy.ndarray object without duplicates.
+    """
+    # Type validation section
+    assert type(a) == np.ndarray
+    assert a.shape == (9, )
+
+    d = np.array([for i in set(range(1, 10)) - set(a)), dtype=np.int64)
+    rnd.shuffle(d)
+
+    return d
 
 get_field = lambda : np.array([np.array([rnd.randint(1, 9) for j in range(9)]) for i in range(9)])
-get_delta = lambda a: np.array([np.int64(i) for i in set(range(1, 10)) - set(a)], dtype=np.int64)
-
